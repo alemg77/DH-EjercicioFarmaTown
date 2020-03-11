b2598f61555c01145bba4c893f1848a9d3ad584b
Alemg <alemg77@yahoo.com.ar>
2020-03-11 14:13:04 -0300

    Commit desde Kraken

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio del programa DH-EjecicioFarmaTown");
+        System.out.println("Mas texto");
+	// write your code here
+    }
+}
---
+++ src/com/company/Medicamento.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public abstract class Medicamento {
+    public String nombreMedicamento;
+    public String nombreDroga;
+    public Integer precio;
+}
